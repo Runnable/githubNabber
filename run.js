a3c6d16cc77e87df00fa6f87bef32c67c3d76ee9
generalhenry <htilford@gmail.com>
2014-02-11 17:47:28 -0800

    verbose run

--- run.js
+++ run.js
@@ -3,7 +3,8 @@ var nab = require('./');
 nab({
   source: 'https://github.com/tardate/jtab',
   destination: 'http://172.16.42.43:4243/build',
-  stack: 'web'
+  stack: 'web',
+  verbose: true
 }, function (err) {
   if (err) {
     throw err;
